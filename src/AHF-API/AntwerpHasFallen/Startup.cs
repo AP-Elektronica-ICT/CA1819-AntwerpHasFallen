ec707a349a4370aae756c90692ae7c1ab414178c
Jorren schepers <s094476@ap.be>
2018-10-18 14:02:23 +0200

    added dbinitialize to startup

--- src/AHF-API/AntwerpHasFallen/Startup.cs
+++ src/AHF-API/AntwerpHasFallen/Startup.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DataLayer;
+using BusinessLayer;
 
 namespace AntwerpHasFallen
 {
@@ -23,16 +26,24 @@ namespace AntwerpHasFallen
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GameContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")
+                )
+            );
+
+            services.AddScoped<GameService>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, GameContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            DbInitializer.Initialize(context);
 
             app.UseMvc();
         }

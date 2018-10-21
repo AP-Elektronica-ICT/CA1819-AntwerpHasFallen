0d9a83bc119d58740f40a62a89d1a508e6f5e223
Jorren schepers <s094476@ap.be>
2018-10-21 19:08:53 +0200

    added functionality to start button, creating game

--- src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/MainActivity.java
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/MainActivity.java
@@ -1,22 +1,53 @@
 package com.ahf.antwerphasfallen;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.support.v4.content.ContextCompat;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    public static final GameDataService service = RetrofitInstance.getRetrofitInstance().create(GameDataService.class);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnStart = (Button)findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateNewGameDialog dialog = new CreateNewGameDialog();
+                dialog.show(getSupportFragmentManager(), "Create new game");
+                /*Game game = new Game();
+                game.getTeams().add(new Team("TestTeamApp1"));
+                game.getTeams().add(new Team("TestTeamApp2"));
+                createNewGame(game);*/
+            }
+        });
 
         /*LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -48,5 +79,34 @@ public class MainActivity extends AppCompatActivity {
         {
             Log.e("error", e.toString());
         }*/
+    }
+
+    @NonNull
+    private String[] getTeamNames(Game game) {
+        List<Team> teams = game.getTeams();
+        String[] teamNames = new String[teams.size()];
+        for(int i = 0; i < teams.size(); i++)
+            teamNames[i] = teams.get(i).getName();
+        return teamNames;
+    }
+
+    public void createNewGame(Game game) {
+        String[] teamNames = getTeamNames(game);
+        Call<Game> call = service.newGame(game.getTeams().size(), teamNames);
+        call.enqueue(new Callback<Game>() {
+            @Override
+            public void onResponse(Call<Game> call, Response<Game> response) {
+                Game game = response.body();
+                Log.d(TAG, "onResponse: " + game.getId());
+                Intent intent = new Intent(MainActivity.this, InGameActivity.class);
+                intent.putExtra("gameId", game.getId());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFailure(Call<Game> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + call.toString());
+            }
+        });
     }
 }

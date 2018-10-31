833580a9957f2a2ec556b2fbe64255cda2f36053
Jorren schepers <s094476@ap.be>
2018-10-31 16:41:23 +0100

    added TeamService

---
+++ src/AHF-API/BusinessLayer/TeamService.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer;
+using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class TeamService
+    {
+        private readonly GameContext context;
+
+        public TeamService(GameContext context)
+        {
+            this.context = context;
+        }
+
+        public Player JoinTeam(int gameId, int teamId)
+        {
+            Game game = context.Games.Find(gameId);
+            Team team = context.Teams.Find(teamId);
+            Player player = new Player() { Game = game, TeamId = team.Id };
+            context.Players.Add(player);
+            context.SaveChanges();
+            return player;
+        }
+    }
+}

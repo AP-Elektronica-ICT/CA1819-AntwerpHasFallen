7c4db9d591b75adf8e054854a66b560f6e347106
Unknown <metsstijn@gmail.com>
2018-11-04 13:10:24 +0100

    changed the location to be requested on interval

--- src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/MainActivity.java
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/MainActivity.java
@@ -16,6 +16,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -24,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private FusedLocationProviderClient mFusedLocationClient;
     private Button btnStart;
     private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
+    private LocationCallback mLocationCallback;
+    private LocationRequest mLocationRequest;
+    private boolean allowLocation = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,30 +43,75 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startLocationUpdates();
             }
         });
+
+
+        //define the location request
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+        //Check if the app has permission to use location.
         if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
+            //If the app doesn't have permission to use location ask if it can.
             ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_CONTACTS},
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_FINE_LOCATION);
         }
         else{
+            allowLocation = true;
+            //If the app already has permission get the location.
             mFusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                         @Override
                         public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            Log.e("locatie","test");
                             if (location != null) {
-                                // Logic to handle location object
+                                // Handle the location object
                                 Log.e("locatie", location.toString());
                             }
                         }
                     });
+
+
         }
 
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                    Log.e("location", location.toString());
+                }
+            };
+        };
+    }
+    /*
+    @Override
+    protected void onResume(){
+        super.onResume();
+        startLocationUpdates();
+
+    }
+    */
+    private void startLocationUpdates(){
+        if(allowLocation){
+            try {
+                mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+            }
+            catch (SecurityException e)
+            {
+                Log.e("Security Exception", e.toString());
+            }
+        }
 
     }
 
@@ -69,21 +120,31 @@ public class MainActivity extends AppCompatActivity {
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+                    allowLocation= true;
+                    /*
+
+                        Code that needs to be executed if the permission is granted.
+
+                    */
                     
                 } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    allowLocation = false;
+                    /*
+
+                        Code that needs to be executed if the permission is denied.
+
+                    */
                 }
                 return;
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request.
+            /*
+
+                Other permissions the app needs
+
+            */
         }
     }
 }

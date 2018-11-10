cc4b796221eafbf2af6ff192d9c37798b6191932
Jorren schepers <s094476@ap.be>
2018-11-10 15:23:42 +0100

    added teamcontroller with basic functionality

---
+++ src/AHF-API/AntwerpHasFallen/Controllers/TeamController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntwerpHasFallen.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/teams")]
+    public class TeamController : Controller
+    {
+        private readonly TeamService teamService;
+
+        public TeamController(TeamService teamService)
+        {
+            this.teamService = teamService;
+        }
+
+        [HttpGet]
+        public IActionResult getAllTeams()
+        {
+            return Ok(teamService.GetTeams());
+        }
+
+        [HttpGet("/players")]
+        public IActionResult getAllPlayers()
+        {
+            return Ok(teamService.GetPlayers());
+        }
+    }
+}

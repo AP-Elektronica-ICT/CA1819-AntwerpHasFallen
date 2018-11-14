d86d04166d6c2a9ac5c65f7cca92674ef7601c26
Jorren schepers <s094476@ap.be>
2018-11-14 11:16:32 +0100

    added button for ending game and updated ingame activity with currentplayer

--- src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/InGameActivity.java
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/InGameActivity.java
@@ -2,9 +2,20 @@ package com.ahf.antwerphasfallen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class InGameActivity extends AppCompatActivity {
+
+
+    public static final GameDataService service = RetrofitInstance.getRetrofitInstance().create(GameDataService.class);
+
+    public Player CurrentPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +28,48 @@ public class InGameActivity extends AppCompatActivity {
         if(extras != null){
             gameId = extras.getInt("gameId");
             playerId = extras.getInt("playerId");
+            loadPlayer(playerId);
         }
 
         TextView txtGameId = (TextView)findViewById(R.id.txt_gameId);
         txtGameId.setText("Game id: " + gameId + "\nPlayer id: " + playerId);
+
+        Button btn_endGame = findViewById(R.id.btn_endGame);
+        btn_endGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void loadPlayer(int id) {
+        Call<Player> call = service.getPlayer(id);
+        call.enqueue(new Callback<Player>() {
+            @Override
+            public void onResponse(Call<Player> call, Response<Player> response) {
+                CurrentPlayer = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<Player> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void EndGame(){
+        Call<Boolean> call = service.endGame(CurrentPlayer.getGameId());
+        call.enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                
+            }
+
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+
+            }
+        });
     }
 }

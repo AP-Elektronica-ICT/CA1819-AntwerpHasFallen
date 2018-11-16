e6f028b4bfe10a53c18a62c517d47adc1efec730
Jorren schepers <s094476@ap.be>
2018-11-16 17:33:35 +0100

    added layout to inventoryfragment and set adapters to listviews

--- src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/InventoryFragment.java
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/InventoryFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -19,7 +22,9 @@ import android.view.ViewGroup;
  */
 public class InventoryFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private InGameActivity host;
+    private IngredientsListAdapter ingredientsAdapter;
+    private ShopInventoryListAdapter shopInventoryAdapter;
 
     public InventoryFragment() {
     }
@@ -40,18 +45,28 @@ public class InventoryFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }*/
+        ingredientsAdapter = new IngredientsListAdapter(getContext(), (ArrayList)host.CurrentTeam.getInventory().getIngredients());
+        shopInventoryAdapter = new ShopInventoryListAdapter(getContext(), (ArrayList)host.CurrentTeam.getInventory().getShopItems());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inventory, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_inventory, container, false);
+
+        ListView lvIngredients = fragmentView.findViewById(R.id.lv_ingredients);
+        lvIngredients.setAdapter(ingredientsAdapter);
+
+        ListView lvShopItems = fragmentView.findViewById(R.id.lv_shopItems);
+        lvShopItems.setAdapter(shopInventoryAdapter);
+
+        return fragmentView;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof InGameActivity) {
+            host = (InGameActivity) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -61,21 +76,6 @@ public class InventoryFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        host = null;
     }
 }

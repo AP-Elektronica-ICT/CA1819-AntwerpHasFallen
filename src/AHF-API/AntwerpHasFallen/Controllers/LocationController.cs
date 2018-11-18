baec05b261ab748b0b2337c2aa7ec332406c699b
Jorren schepers <s094476@ap.be>
2018-11-18 16:26:06 +0100

    added locationscontroller

---
+++ src/AHF-API/AntwerpHasFallen/Controllers/LocationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntwerpHasFallen.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/locations")]
+    public class LocationController : Controller
+    {
+
+    }
+}

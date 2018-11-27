0844ca66cc7d5b258cbc278f8c779a7ad0cb7157
Unknown <metsstijn@gmail.com>
2018-11-27 11:24:58 +0100

    changed menu colors

--- src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/InGameActivity.java
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/InGameActivity.java
@@ -1,6 +1,9 @@
 package com.ahf.antwerphasfallen;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -14,6 +17,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;

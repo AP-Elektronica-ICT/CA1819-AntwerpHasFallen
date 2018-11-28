262769993501ee34b24d92365cb91135f57181e0
Jorren schepers <s094476@ap.be>
2018-11-28 18:40:19 +0100

    updated classes in app accordingly to database changes

---
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/Item.java
@@ -0,0 +1,30 @@
+package com.ahf.antwerphasfallen;
+
+/**
+ * Created by Jorren on 28/11/2018.
+ */
+
+public abstract class Item {
+    private int id;
+    private String name;
+
+    public Item() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}

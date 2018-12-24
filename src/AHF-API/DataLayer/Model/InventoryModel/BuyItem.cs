615319f14fdbf0bd7e553964b211a7dd63966108
Jorren schepers <s094476@ap.be>
2018-12-24 11:32:31 +0100

    added class for buyable item

---
+++ src/AHF-API/DataLayer/Model/InventoryModel/BuyItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Model.InventoryModel
+{
+    public class BuyItem
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public Item Item { get; set; }
+    }
+}
---
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/BuyItem.java
@@ -0,0 +1,24 @@
+package com.ahf.antwerphasfallen;
+
+/**
+ * Created by Jorren on 24/12/2018.
+ */
+
+public class BuyItem {
+
+    private int id;
+    private int price;
+    private Item item;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+}

07e52db15b89bbe177236fbb0323612bdc7ed3db
Jorren schepers <s094476@ap.be>
2018-12-30 10:40:19 +0100

    removed id property from previouslocation class

--- src/AHF-API/DataLayer/Model/PreviousLocation.cs
+++ src/AHF-API/DataLayer/Model/PreviousLocation.cs
@@ -6,7 +6,6 @@ namespace DataLayer.Model
 {
     public class PreviousLocation
     {
-        public int Id { get; set; }
         public int TeamId { get; set; }
         public Team Team { get; set; }
         public int LocationId { get; set; }

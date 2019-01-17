d03a70ee71e9bb6b5eafd14b8e162cd8e262be88
Jorren schepers <s094476@ap.be>
2019-01-17 19:32:31 +0100

    added class for finished game, to keep stats

---
+++ src/AHF-API/DataLayer/Model/FinishedGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Model
+{
+    public class FinishedGame
+    {
+        public FinishedGame(int gameId)
+        {
+            GameId = gameId;
+        }
+
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public string TeamsLeaderboard { get; set; }
+        public string Winner { get; set; }
+    }
+}

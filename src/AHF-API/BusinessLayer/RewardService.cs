115ec2546dca57d6aa9668d5b0953f331b7928d9
Jorren schepers <s094476@ap.be>
2019-01-19 19:21:42 +0100

    included item in ingredients

--- src/AHF-API/BusinessLayer/RewardService.cs
+++ src/AHF-API/BusinessLayer/RewardService.cs
@@ -23,7 +23,7 @@ namespace BusinessLayer
 
         public Team Reward(int teamId, string difficulty, bool answer, bool gotIngredient, List<string> missingIngredients)
         {
-            Team team = context.Teams.Include(t => t.Inventory).ThenInclude(i => i.Ingredients).SingleOrDefault(t => t.Id == teamId);
+            Team team = context.Teams.Include(t => t.Inventory).ThenInclude(i => i.Ingredients).ThenInclude(i => i.Item).SingleOrDefault(t => t.Id == teamId);
             List<Item> missingIngredientsList = new List<Item>();
             Random r = new Random();
 

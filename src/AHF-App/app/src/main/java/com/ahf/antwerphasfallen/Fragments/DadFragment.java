9516ca08dedf9cc4e2ee6b8f4e3eacf452c0d202
JanekKemp <kempeneers.janek@gmail.com>
2019-01-19 12:49:01 +0100

    drag and drop

--- src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/Fragments/DadFragment.java
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/Fragments/DadFragment.java
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ahf.antwerphasfallen.Helpers.GameDataService;
 import com.ahf.antwerphasfallen.Helpers.RetrofitInstance;
@@ -31,13 +33,17 @@ public class DadFragment extends Fragment {
     private EditText sp2;
     private EditText sp3;
     private EditText sp4;
-
+    private Boolean found;
+    private List<String> order;
+    private List<String> answers;
     private TextView t1;
     private TextView t2;
     private TextView t3;
     private TextView t4;
+    private int count = 1;
     private TextView question;
     private Button check;
+    private String answer;
 
     String location ="MAS";
     String correctorder;
@@ -94,12 +100,71 @@ public class DadFragment extends Fragment {
         sp2 = (EditText) rootView.findViewById(R.id.Spinner2);
         sp3 = (EditText) rootView.findViewById(R.id.Spinner3);
         sp4 = (EditText) rootView.findViewById(R.id.Spinner4);
+        check = (Button) rootView.findViewById(R.id.solution);
         question = (TextView) rootView.findViewById(R.id.question);
         t1 = (TextView) rootView.findViewById(R.id.Choice1);
         t2 = (TextView) rootView.findViewById(R.id.Choice2);
         t3 = (TextView) rootView.findViewById(R.id.Choice3);
         t4 = (TextView) rootView.findViewById(R.id.Choice4);
         //sp1.get
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                order.add(sp1.getText().toString());
+                order.add(sp2.getText().toString());
+                order.add(sp3.getText().toString());
+                order.add(sp4.getText().toString());
+
+                answers.add(t1.getText().toString());
+                answers.add(t2.getText().toString());
+                answers.add(t3.getText().toString());
+                answers.add(t4.getText().toString());
+
+                for (int i = 0;i<3;i++)
+                {
+                    if(order.get(i).toString() == "1")
+                    {
+                        answer = answers.get(i).toString();
+                    }
+                }
+                for (int i = 0;i<3;i++)
+                {
+                    if(order.get(i).toString() == "2")
+                    {
+                        answer = answer + "," + answers.get(i).toString();
+                    }
+                }
+                for (int i = 0;i<3;i++)
+                {
+                    if(order.get(i).toString() == "3")
+                    {
+                        answer = answer + "," + answers.get(i).toString();
+                    }
+                }
+                for (int i = 0;i<3;i++)
+                {
+                    if(order.get(i).toString() == "4")
+                    {
+                        answer = answer + "," + answers.get(i).toString();
+                    }
+                }
+
+                if (solution.equals(answer)){
+
+
+                    Toast.makeText(host, "Correct",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(host, "Wrong",Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+
+        });
         return rootView;
     }
 }

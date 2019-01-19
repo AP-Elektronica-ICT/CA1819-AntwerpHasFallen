db89d6123ca2496290da78607097c1a54452ca77
Jorren schepers <s094476@ap.be>
2019-01-19 14:40:27 +0100

    removed unnessecary services and added service for rewards

---
+++ src/AHF-API/BusinessLayer/RewardService.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class RewardService
+    {
+        private readonly GameContext context;
+
+        public RewardService(GameContext context)
+        {
+            this.context = context;
+        }
+    }
+}

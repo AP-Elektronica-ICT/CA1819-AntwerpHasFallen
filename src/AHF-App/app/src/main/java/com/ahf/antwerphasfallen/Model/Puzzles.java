c56e8af759ab18657a13c0b36997c4cf2f8ec5e5
JanekKemp <kempeneers.janek@gmail.com>
2019-01-21 20:38:23 +0100

    minor fixex

--- src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/Model/Puzzles.java
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/Model/Puzzles.java
@@ -174,7 +174,7 @@ public class Puzzles extends Fragment {
     private void SubAlert()
     {
         builder = new AlertDialog.Builder(listener);
-        builder.setTitle("Dit is een zeer tijdsrovende puzzel! Maar u zal dan ook rijkelijk beloond worden!")
+        builder.setTitle("Dit is een zeer tijdsrovende puzzel!")
                 .setMessage(R.string.vignere)
                 .setIcon(android.R.drawable.ic_dialog_info)
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {

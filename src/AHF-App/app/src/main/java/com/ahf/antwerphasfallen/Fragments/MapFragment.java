294f47ecbccaa6493d57f6767930c56bde717583
Unknown <metsstijn@gmail.com>
2019-01-21 22:26:42 +0100

    location comment

--- src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/Fragments/MapFragment.java
+++ src/AHF-App/app/src/main/java/com/ahf/antwerphasfallen/Fragments/MapFragment.java
@@ -207,7 +207,7 @@ public class MapFragment extends Fragment {
         d = Math.round(d*1000);
         Log.e("distance", d + "m");
         //txt_distance.setText(d + "m");
-        d = 10;
+        //d = 10;
         if(d <= 20){
             builder.show();
             mFusedLocationClient.removeLocationUpdates(mLocationCallback);
